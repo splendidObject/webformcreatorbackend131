3bce4366a9fc1f40cca5e59190f827bd50371a9e
splendidObject <lincolnbartlett@csus.edu>
2022-04-14 16:54:44 -0700

    fixed mongoose connect

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.use(morgan('tiny'));
 
 //MONGO - database
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017');
-//mongoose.connect(process.env.MONGODB_URI)
+//mongoose.connect('mongodb://localhost:27017');
+mongoose.connect(process.env.MONGODB_URI)
 
 
 
