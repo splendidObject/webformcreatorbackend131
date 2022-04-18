b3151de2995565ad48a9ad6c2d7a51a07e362da6
splendidObject <lincolnbartlett@csus.edu>
2022-04-17 19:17:58 -0700

    added new build

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ app.use(morgan('tiny'));
 
 //MONGO - database
 const mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost:27017');
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017')
 
 
 
